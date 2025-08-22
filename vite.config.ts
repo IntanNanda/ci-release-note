27e9fd89a1d21282be037c8e6ba2098f7d56a677
IntanNanda <intan.irnanda@sera.astra.co.id>
2025-08-22 18:44:34 +0700

    update vite #3

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   base: process.env.NODE_ENV === 'production' ? '/ci-release-note/' : '/',
+  define: {
+    'import.meta.env.VITE_CONFLUENCE_EMAIL': JSON.stringify(process.env.VITE_CONFLUENCE_EMAIL),
+    'import.meta.env.VITE_CONFLUENCE_API_TOKEN': JSON.stringify(process.env.VITE_CONFLUENCE_API_TOKEN),
+    'import.meta.env.VITE_APIM': JSON.stringify(process.env.VITE_APIM),
+  },
 });
